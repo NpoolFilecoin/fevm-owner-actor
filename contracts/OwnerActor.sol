4a0515a22829aa7e833b64197ad8acd27a3b7b94
kikakkz <kikakkz@hotmail.com>
2023-01-31 19:14:24 +0800

    Add parameter to constructor

--- contracts/OwnerActor.sol
+++ contracts/OwnerActor.sol
@@ -8,9 +8,25 @@ import './miner/Miner.sol';
 /// @notice Owner actor implementation of Filecoin miner
 contract OwnerActor {
     address public creator;
+    enum CustodyType {
+        FIXED_INCOME,
+        FIXED_FEE_RATE
+    }
+    CustodyType custodyType = CustodyType.FIXED_FEE_RATE;
+    uint8 benefitValue = 0;
 
-    constructor() {
+    constructor(string memory _custodyType, uint8 value) {
         creator = msg.sender;
+        if (keccak256(bytes(_custodyType)) == keccak256(bytes("FixedIncome"))) {
+            custodyType = CustodyType.FIXED_INCOME;
+        } else if (keccak256(bytes(_custodyType)) == keccak256(bytes("FixedFeeRate"))) {
+            custodyType = CustodyType.FIXED_FEE_RATE;
+        } else {
+            revert("Invalid custody type");
+        }
+
+        require(value < 100, "Invalid custody value");
+        benefitValue = value;
     }
 
     /// @notice Specific function let invoker know this is a peggy contract
