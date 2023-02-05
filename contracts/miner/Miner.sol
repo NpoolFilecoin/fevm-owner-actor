6507a36a8cd363facf221b0e0916ff37ea7a238d
kikakkz <kikakkz@hotmail.com>
2023-02-05 18:09:56 +0800

    Avoid call error

--- contracts/miner/Miner.sol
+++ contracts/miner/Miner.sol
@@ -42,25 +42,24 @@ library Miner {
 
     function initializeInfo(_Miner storage miner) internal {
         MinerTypes.GetAvailableBalanceReturn memory ret1 = MinerAPI.getAvailableBalance(miner.minerId);
-        // uint256 initialAvailable = Bytes2Uint.toUint256(ret1.available_balance.val);
-        uint256 initialAvailable = 0;
+        uint256 initialAvailable = Bytes2Uint.toUint256(ret1.available_balance.val);
         if (ret1.available_balance.neg) {
             miner.initialAvailable = int256(initialAvailable) * -1;
         } else {
             miner.initialAvailable = int256(initialAvailable);
         }
 
-        /*
+        /* TODO: commented due to this call is error
         MinerTypes.GetVestingFundsReturn memory ret2 = MinerAPI.getVestingFunds(miner.minerId);
         uint256 initialVesting = 0;
         for (uint32 i = 0; i < ret2.vesting_funds.length; i++) {
             initialVesting += Bytes2Uint.toUint256(ret2.vesting_funds[i].amount.val);
         }
         miner.initialVesting = initialVesting;
+        */
 
         PowerTypes.MinerRawPowerReturn memory ret3 = PowerAPI.minerRawPower(miner.minerId);
         miner.initialRawPower = Bytes2Uint.toUint256(ret3.raw_byte_power.val);
-        */
     }
 
     function setFeeBeneficiaries(
--- contracts/utils/Bytes2Uint.sol
+++ contracts/utils/Bytes2Uint.sol
@@ -7,7 +7,19 @@ library Bytes2Uint {
     *  @return      uint256
     */
     function toUint256(bytes memory _bytes) internal pure returns (uint256) {
-        require(_bytes.length >= 32, "toUint256_outOfBounds");
+        uint256 number;
+        for (uint i = 0; i < _bytes.length; i++) {
+            number = number + uint(uint8(_bytes[i])) * (2 ** (8 * (_bytes.length - (i + 1))));
+        }
+        return number;
+    }
+
+    /* @notice      Convert bytes to uint256
+    *  @param _b    Source bytes must have length of 32
+    *  @return      uint256
+    */
+    function toUint256Opt(bytes memory _bytes) internal pure returns (uint256) {
+        require(_bytes.length == 32, "toUint256_outOfBounds");
         uint256 tempUint;
         assembly {
             tempUint := mload(add(add(_bytes, 0x20), 0))
