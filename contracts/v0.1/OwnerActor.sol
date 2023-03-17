46a691d29a57ae8a7523fdde1569e6c34370226e
kikakkz <kikakkz@hotmail.com>
2023-03-17 12:42:20 +0800

    Support V0.1 smart contract

--- contracts/v0.1/OwnerActor.sol
+++ contracts/v0.1/OwnerActor.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity = 0.8.17;
+pragma solidity ^0.8.17;
 
-import "./beneficiary/Beneficiary.sol";
-import "./miner/Miner.sol";
-import "./utils/Uint2Str.sol";
+import "./controller/Controllable.sol";
 
-import "https://github.com/Zondax/filecoin-solidity/blob/master/contracts/v0.8/PowerAPI.sol";
-import "https://github.com/Zondax/filecoin-solidity/blob/master/contracts/v0.8/types/PowerTypes.sol";
+// import "https://github.com/Zondax/filecoin-solidity/blob/v4.0.2/contracts/v0.8/PowerAPI.sol";
+// import "https://github.com/Zondax/filecoin-solidity/blob/v4.0.2/contracts/v0.8/types/PowerTypes.sol";
 
 // TODO: we cannot detect method 0 within runtime contract
 //     so we have to let a genesis account to record the deposit
@@ -15,33 +13,10 @@ import "https://github.com/Zondax/filecoin-solidity/blob/master/contracts/v0.8/t
 
 /// @title FEVM Owner actor
 /// @notice Owner actor implementation of Filecoin miner
-contract OwnerActor {
-    address public creator;
-    enum CustodyType {
-        FIXED_INCOME,
-        FIXED_FEE_RATE
-    }
-    CustodyType custodyType = CustodyType.FIXED_FEE_RATE;
-    uint8 benefitValue = 0;
-    mapping(uint64 => Miner._Miner) miners;
-    uint64[] minerIds;
-    mapping(address => Beneficiary.RewardBeneficiary) rewardBeneficiaries;
-    address[] rewardAddresses;
+contract OwnerActor is Controllable {
+    address private _storageProvider;
 
-    event RawPowerReturn(PowerTypes.MinerRawPowerReturn ret);
-
-    constructor(string memory _custodyType, uint8 value) {
-        creator = msg.sender;
-        if (keccak256(bytes(_custodyType)) == keccak256(bytes("FixedIncome"))) {
-            custodyType = CustodyType.FIXED_INCOME;
-        } else if (keccak256(bytes(_custodyType)) == keccak256(bytes("FixedFeeRate"))) {
-            custodyType = CustodyType.FIXED_FEE_RATE;
-        } else {
-            revert("Invalid custody type");
-        }
-
-        require(value < 100, "Invalid custody value");
-        benefitValue = value;
+    constructor() {
     }
 
     /// @notice Specific function let invoker know this is a peggy contract
@@ -50,121 +25,15 @@ contract OwnerActor {
         return "Peggy TZJCLSYW 09231006 .--././--./--./-.--/-/--../.---/-.-./.-../.../-.--/.--/-----/----./..---/...--/.----/-----/-----/-....";
     }
 
-    function playPeggy(uint64 minerId) public {
-        emit RawPowerReturn(PowerAPI.minerRawPower(minerId));
+    function addController(address controller) public onlyController {
+        _addController(controller);
     }
 
-    function whoAmI() public view returns (string memory) {
-        string memory str = "{";
-
-        str = string(bytes.concat(bytes(str), bytes("\"CustodyType\":\"")));
-        if (custodyType == CustodyType.FIXED_INCOME) {
-            str = string(bytes.concat(bytes(str), bytes("FixedIncome")));
-        } else if (custodyType == CustodyType.FIXED_FEE_RATE) {
-            str = string(bytes.concat(bytes(str), bytes("FixedFeeRate")));
-        }
-        
-        str = string(bytes.concat(bytes(str), bytes("\",\"BenefitValue\":\"")));
-        str = string(bytes.concat(bytes(str), bytes(Uint2Str.toString(benefitValue))));
-
-        string memory rewardBeneficiary = "[";
-        for (uint32 i = 0; i < rewardAddresses.length; i++) {
-            Beneficiary.RewardBeneficiary memory value = rewardBeneficiaries[rewardAddresses[i]];
-
-            if (i > 0) {
-                rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes(",")));
-            }
-
-            rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes("{\"Address\":\"")));
-            rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), abi.encode(value.beneficiary)));
-
-            rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes("\",\"Amount\":\"")));
-            rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes(Uint2Str.toString(value.amount))));
-
-            rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes("\"}")));
-        }
-        rewardBeneficiary = string(bytes.concat(bytes(rewardBeneficiary), bytes("]")));
-
-        str = string(bytes.concat(bytes(str), bytes("\",\"RewardBeneficiaries\":")));
-        str = string(bytes.concat(bytes(str), bytes(rewardBeneficiary)));
-
-        str = string(bytes.concat(bytes(str), bytes("}")));
-        return str;
+    function deleteController(address controller) public onlyController {
+        _deleteController(controller);
     }
 
-    function setInitialRewardBeneficiaries(
-        uint256 totalAmount,
-        Beneficiary.FeeBeneficiary[] memory beneficiaries
-    ) internal {
-        for (uint i = 0; i < beneficiaries.length; i++) {
-            Beneficiary.FeeBeneficiary memory beneficiary = beneficiaries[i];
-            rewardBeneficiaries[beneficiary.beneficiary].beneficiary = beneficiary.beneficiary;
-            rewardBeneficiaries[beneficiary.beneficiary].amount = totalAmount * beneficiary.percent;
-            rewardAddresses.push(beneficiary.beneficiary);
-        }
-    }
-
-
-    /// @notice Change Owner of specific miner to this running contract with initial condition
-    function custodyMiner(
-        uint64 minerId,
-        Beneficiary.FeeBeneficiary[] memory feeBeneficiaries,
-        Beneficiary.FeeBeneficiary[] memory _rewardBeneficiaries
-    ) public {
-        Miner._Miner storage miner = miners[minerId];
-        require(!miner.exist, "Exist miner");
-
-        Miner.init(miner, minerId);
-        Miner.initializeInfo(miner);
-
-        require(miner.initialAvailable >= 0, "Debt miner");
-
-        Miner.setFeeBeneficiaries(miner, feeBeneficiaries);
-
-        uint256 totalAmount = miner.initialCollateral;
-
-        uint8 totalPercent = 0;
-        for (uint i = 0; i < _rewardBeneficiaries.length; i++) {
-            require(_rewardBeneficiaries[i].percent > 0 && _rewardBeneficiaries[i].percent <= 100, "Invalid percent");
-            totalPercent += _rewardBeneficiaries[i].percent;
-        }
-
-        if (totalAmount > 0) {
-            require(totalPercent == 100, "Invalid total percent");
-        }
-
-        setInitialRewardBeneficiaries(totalAmount, _rewardBeneficiaries);
-        Miner.custody(miner);
-
-        miner.exist = true;
-        minerIds.push(minerId);
-    }
-
-    /// @notice Get miner entity with minerId
-    function getMiner(uint64 minerId) public view returns (string memory) {
-        Miner._Miner storage miner = miners[minerId];
-        require(miner.exist, "Invalid miner");
-
-        return Miner.toString(miner);
-    }
-
-    /// @notice Get all miners
-    function getMiners() public view returns (string memory) {
-        string memory minersStr = "[";
-        for (uint i = 0; i < minerIds.length; i++) {
-            Miner._Miner storage miner = miners[minerIds[i]];
-            if (i > 0) {
-                minersStr = string(bytes.concat(bytes(minersStr), bytes(",")));
-            }
-            string memory minerStr = Miner.toString(miner);
-            minersStr = string(bytes.concat(bytes(minersStr), bytes(minerStr)));
-        }
-        minersStr = string(bytes.concat(bytes(minersStr), bytes("]")));
-        return minersStr;
-    }
-
-    /// @notice Get miner ids custodied in this contract
-    function getMinerIds() public view returns (uint64[] memory) {
-        return minerIds;
+    function confirmController() public {
+        _confirmController();
     }
 }
---
+++ contracts/v0.1/controller/Controllable.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.17;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Controllable {
+    mapping(address => bool) private _controllers;
+    address[] private __controllers;
+    uint8 private _controllers_count;
+    mapping(address => uint8) private _addingControllers;
+    mapping(address => uint8) private _deletingControllers;
+    mapping(address => mapping(address => bool)) private _addingApprovers;
+    mapping(address => mapping(address => bool)) private _deletingApprovers;
+    mapping(address => address[]) private __addingApprovers;
+    mapping(address => address[]) private __deletingApprovers;
+
+    event ControllerAdded(address indexed proposer, address indexed controller);
+    event ControllerDeleted(address indexed approver, address indexed controller);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial controller.
+     */
+    constructor () {
+        address msgSender = msg.sender;
+        _controllers[msgSender] = true;
+        __controllers.push(msgSender);
+        _controllers_count += 1;
+        emit ControllerAdded(msgSender, msgSender);
+    }
+
+    /**
+     * @dev Returns the addresses of the current controllers.
+     */
+    function controllers() public view returns (address[] memory) {
+        return __controllers;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the controllers.
+     */
+    modifier onlyController() {
+        require(isController(), "Controllable: caller is not the controller");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current controllers.
+     */
+    function isController() public view returns (bool) {
+        address msgSender = msg.sender;
+        return _controllers[msgSender];
+    }
+
+    /**
+     * @dev Propose to add a new controller
+     */
+    function _addController(address controller) public onlyController {
+        address msgSender = msg.sender;
+        require(!_controllers[controller], "Controllable: address is already a controller");
+        require(_deletingControllers[controller] == 0, "Controllable: address is deleting");
+        require(!_addingApprovers[controller][msgSender], "Controllable: sender already approved");
+        _addingControllers[msgSender] += 1;
+        _addingApprovers[controller][msgSender] = true;
+        __addingApprovers[controller].push(msgSender);
+        emit ControllerAdded(msgSender, controller);
+    }
+
+    /**
+     * @dev Propose to delete a new controller
+     */
+    function _deleteController(address controller) public onlyController {
+        address msgSender = msg.sender;
+        require(_controllers[controller], "Controllable: address is not a controller");
+        require(_addingControllers[controller] == 0, "Controllable: address is adding");
+        require(_deletingControllers[controller] == 0, "Controllable: address is deleting");
+        require(!_deletingApprovers[controller][msgSender], "Controllable: sender already approved");
+        _deletingControllers[controller] += 1;
+        _deletingApprovers[controller][msgSender] = true;
+        __deletingApprovers[controller].push(msgSender);
+        if (_deletingControllers[controller] > _controllers_count * 2 / 3) {
+            _controllers_count -= 1;
+            _deletingControllers[controller] = 0;
+            _controllers[controller] = false;
+
+            for (uint8 i = 0; i < __deletingApprovers[controller].length; i++) {
+                _deletingApprovers[controller][__deletingApprovers[controller][i]] = false;
+            }
+            for (uint8 i = 0; i < __deletingApprovers[controller].length; i++) {
+                __deletingApprovers[controller].pop();
+            }
+        }
+        emit ControllerDeleted(msgSender, controller);
+    }
+
+    /**
+     * @dev Confirm to be a controller
+     */
+    function _confirmController() public {
+        address msgSender = msg.sender;
+        require(!_controllers[msgSender], "Controllable: address is already a controller");
+        require(_addingControllers[msgSender] > _controllers_count * 2 / 3, "Controllable: address is voting");
+        require(_deletingControllers[msgSender] == 0, "Controllable: address is deleting");
+        _controllers_count += 1;
+        _addingControllers[msgSender] = 0;
+        _controllers[msgSender] = true;
+        __controllers.push(msgSender);
+        for (uint8 i = 0; i < __addingApprovers[msgSender].length; i++) {
+            _addingApprovers[msgSender][__addingApprovers[msgSender][i]] = false;
+        }
+        for (uint8 i = 0; i < __addingApprovers[msgSender].length; i++) {
+            __addingApprovers[msgSender].pop();
+        }
+    }
+}
