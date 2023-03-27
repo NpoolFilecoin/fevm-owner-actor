a9edadcd0f2d2720fc2122d87f15755bffa377ed
Zhao KK <kikakkz@hotmail.com>
2023-03-27 16:33:43 +0800

    Add fvm to v0.1

---
+++ contracts/v0.1/fvm/Types.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity = 0.8.17;
+
+library FvmTypes {
+    enum RegisteredPoStProof {
+        StackedDRGWinning2KiBV1,
+        StackedDRGWinning8MiBV1,
+        StackedDRGWinning512MiBV1,
+        StackedDRGWinning32GiBV1,
+        StackedDRGWinning64GiBV1,
+        StackedDRGWindow2KiBV1,
+        StackedDRGWindow8MiBV1,
+        StackedDRGWindow512MiBV1,
+        StackedDRGWindow32GiBV1,
+        StackedDRGWindow64GiBV1
+    }
+}
