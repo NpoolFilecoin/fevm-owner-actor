855f95a2e997953a52a5b0a8198851bc380f4a66
kikakkz <kikakkz@hotmail.com>
2023-03-27 17:13:03 +0800

    Support to custody miner

--- contracts/v0.1/OwnerActor.sol
+++ contracts/v0.1/OwnerActor.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import "./controller/Controllable.sol";
 import "./miner/Miner.sol";
+import "./beneficiary/Beneficiary.sol";
 
 // import "https://github.com/Zondax/filecoin-solidity/blob/v4.0.2/contracts/v0.8/PowerAPI.sol";
 // import "https://github.com/Zondax/filecoin-solidity/blob/v4.0.2/contracts/v0.8/types/PowerTypes.sol";
@@ -35,5 +36,34 @@ contract OwnerActor is Controllable {
         require(_miner.exist, "Miner: there is no miner custodied");
         string memory minerStr = Miner.toString(_miner);
         return minerStr;
+    }
+
+    /// @notice Change Owner of specific miner to this running contract
+    function custodyMiner(
+        uint64 minerId,
+        Beneficiary.Percent[] memory percentBeneficiaries
+    ) public onlyController {
+        require(!_miner.exist, "Miner: only allow to custody one miner");
+
+        Miner.init(_miner, minerId);
+        Miner.initializeInfo(_miner);
+
+        Miner.setPercentBeneficiaries(_miner, percentBeneficiaries);
+
+        uint256 totalAmount = _miner.initialCollateral;
+
+        uint8 totalPercent = 0;
+        for (uint i = 0; i < percentBeneficiaries.length; i++) {
+            require(percentBeneficiaries[i].percent > 0 && percentBeneficiaries[i].percent <= 100, "Miner: Invalid percent");
+            totalPercent += percentBeneficiaries[i].percent;
+        }
+
+        if (totalAmount > 0) {
+            require(totalPercent == 100, "Miner: Invalid total percent");
+        }
+
+        Miner.custody(_miner);
+
+        _miner.exist = true;
     }
 }
